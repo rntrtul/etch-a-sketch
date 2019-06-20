a0d7a84b59a86d088cc5b43f39120737663d9949
Rntrtul <Sistthelord@gmail.com>
2019-06-20 19:20:35 -0400

    added css and centerd grid

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const grid = document.querySelector("#container");
 var burshState = 0;
+var size = 16;
 
-function drawGrid(size){
+function drawGrid(){
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     } 
@@ -17,43 +18,50 @@ function drawGrid(size){
     addHoverFunction();
 }
 
-drawGrid(16);
+drawGrid(size);
 
-function randomColor() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    return "rgb(" + x + "," + y + "," + z + ")";
+function brushColor(state) {
+    if (state == 0){
+        return "black";
+    }else {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        return "rgb(" + x + "," + y + "," + z + ")";
+    }
 }
 
 function addHoverFunction(){
     window.squares = document.querySelectorAll(".square");
     squares.forEach(element =>{
         element.addEventListener("mouseover", (e) =>{
-            if(burshState == 0){
-             element.classList.add("selected")
-            }else{
-                var color = randomColor();
-                element.style = "background-color:" + color +
-                                "; border: " + color;
-        }
+            var color = brushColor(burshState);
+            element.style = "background-color:" + color + "; border: " + color;
         });
     }); 
 }
 
 const reset = document.querySelector("#reset");
 reset.addEventListener('click', (e) =>{
-    squares.forEach(element => {
-        element.classList.remove("selected");
-    });
+    drawGrid(size);
 });
 
-const size = document.querySelector("#size");
-size.addEventListener('click', (e) =>{
-    drawGrid(prompt("How many columns would you like?"));
+const changeSize = document.querySelector("#size");
+changeSize.addEventListener('click', (e) =>{
+    size = prompt("How many columns would you like?")
+    if(size <=128){
+        drawGrid(size);
+    }    
 });
 
 const rand = document.querySelector("#random");
 rand.addEventListener('click', (e)=>{
-    burshState ==0 ? burshState =1 : burshState = 0;
+    if (burshState == 0){
+        burshState = 1;
+        rand.textContent = "Black";
+    }else{
+        burshState = 0;
+        rand.textContent = "Colours";
+    }
+    
 });
