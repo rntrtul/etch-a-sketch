e3a0e49e5f4a3e4f94d42088c78d3bf9b6801ebd
Rntrtul <Sistthelord@gmail.com>
2019-06-20 18:12:53 -0400

    added random colours button, working

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector("#container");
+var burshState = 0;
 
 function drawGrid(size){
     while (grid.firstChild) {
@@ -7,21 +8,35 @@ function drawGrid(size){
 
     grid.style.gridTemplateColumns = "1fr ".repeat(size);
     grid.style.gridTemplateRows = "1fr ".repeat(size);
-    
+
     for (var i = 0; i < size**2 ;i++){
         const square = document.createElement('div');
         square.className = "square";
         grid.appendChild(square);
     }
-    onHover();
+    addHoverFunction();
 }
-drawGrid(4);
 
-function onHover(){
+drawGrid(16);
+
+function randomColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    return "rgb(" + x + "," + y + "," + z + ")";
+}
+
+function addHoverFunction(){
     window.squares = document.querySelectorAll(".square");
     squares.forEach(element =>{
         element.addEventListener("mouseover", (e) =>{
-            element.classList.add("selected");
+            if(burshState == 0){
+             element.classList.add("selected")
+            }else{
+                var color = randomColor();
+                element.style = "background-color:" + color +
+                                "; border: " + color;
+        }
         });
     }); 
 }
@@ -36,4 +51,9 @@ reset.addEventListener('click', (e) =>{
 const size = document.querySelector("#size");
 size.addEventListener('click', (e) =>{
     drawGrid(prompt("How many columns would you like?"));
+});
+
+const rand = document.querySelector("#random");
+rand.addEventListener('click', (e)=>{
+    burshState ==0 ? burshState =1 : burshState = 0;
 });
