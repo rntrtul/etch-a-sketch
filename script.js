92e9c2597b52f86d9bdf3314824fcd7b6d6c8319
Rntrtul <Sistthelord@gmail.com>
2019-06-20 17:31:10 -0400

    resize grid added

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const grid = document.querySelector("#container");
-var columns = 0;
 
 function drawGrid(size){
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    } 
+    
     for (var i = 0; i < size**2 ;i++){
         const square = document.createElement('div');
         square.className = "square";
         grid.appendChild(square);
     }
+    onHover();
 }
+drawGrid(4);
 
-const squares = document.querySelectorAll(".square");
-
-squares.forEach(element =>{
-    element.addEventListener("mouseover", (e) =>{
-        element.classList.add("selected");
-    });
-});
+function onHover(){
+    window.squares = document.querySelectorAll(".square");
+    squares.forEach(element =>{
+        element.addEventListener("mouseover", (e) =>{
+            element.classList.add("selected");
+        });
+    }); 
+}
 
 const reset = document.querySelector("#reset");
 reset.addEventListener('click', (e) =>{
@@ -25,7 +31,6 @@ reset.addEventListener('click', (e) =>{
 });
 
 const size = document.querySelector("#size");
-reset.addEventListener('click', (e) =>{
-    columns =  prompt("How many columns would you like?");
-    drawGrid(columns);
+size.addEventListener('click', (e) =>{
+    drawGrid(prompt("How many columns would you like?"));
 });
