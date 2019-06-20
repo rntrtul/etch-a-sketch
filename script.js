968b8d80513c70202d76b9a03ef27abc1fd22c7a
Rntrtul <Sistthelord@gmail.com>
2019-06-20 17:11:45 -0400

    hover changes colour and reset button works

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const grid = document.querySelector("#container");
+var columns = 0;
 
-for (var i =0; i < 16;i++){
-    const square = document.createElement('div');
-    square.className = "square ";
-    square.id = i;
-    grid.appendChild(square);
+function drawGrid(size){
+    for (var i = 0; i < size**2 ;i++){
+        const square = document.createElement('div');
+        square.className = "square";
+        grid.appendChild(square);
+    }
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(element =>{
+    element.addEventListener("mouseover", (e) =>{
+        element.classList.add("selected");
+    });
+});
+
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', (e) =>{
+    squares.forEach(element => {
+        element.classList.remove("selected");
+    });
+});
+
+const size = document.querySelector("#size");
+reset.addEventListener('click', (e) =>{
+    columns =  prompt("How many columns would you like?");
+    drawGrid(columns);
+});
